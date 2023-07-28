65b4d1bd42c8ae592554874fffe64f7dff068c1a
KISHAN JAWALE <kishanjawale10@gmail.com>
2023-07-28 11:19:03 +0530

    Add files via upload

---
+++ Assign2_Question3.C
@@ -0,0 +1,36 @@
+//Write a program which accpets directory name from user and 
+//print all file names from tha directory
+#include<stdio.h>
+#include<stdlib.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<sys/stat.h>
+#include<dirent.h>
+
+int main(int argc, char *argv[])
+{
+    DIR *dp = NULL;
+    struct dirent * entry = NULL;
+
+    if(argc != 2)
+    {
+        printf("Insufficient Number of arguments\n");
+        return -1;
+    }
+
+    dp = opendir(argv[1]);
+    if(dp == NULL)
+    {
+        printf("Unable to open directory...\n");
+        return -1;
+    }
+    printf("Entries from the directory file are : \n");
+    
+    while((entry = readdir(dp)) != NULL)
+    {
+        printf("%20s \n",entry->d_name);
+    }
+
+    closedir(dp);
+    return 0;
+}
---
+++ Assign2_Question5.C
@@ -0,0 +1,53 @@
+//Write a program which accpets directory name from user and 
+//print name of such file having largest size.
+    #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<dirent.h>
+#include<sys/stat.h>
+
+int main(int argc, char *argv[])
+{
+    DIR *dp = NULL;
+    struct dirent *entry = NULL;
+    char name[30];
+    struct stat sobj;
+    char namecopy[30] = {'\0'};
+    int iMax = 0;
+
+    if(argc!=2)
+    {
+        printf("Insufficient number of Arguments...\n");
+        return -1;
+    }
+
+    
+    dp = opendir(argv[1]);
+    if(dp == NULL)
+    {
+        printf("Unable to open directory\n");
+        return -1;
+    }
+
+    while((entry = readdir(dp)) != NULL)
+    {
+        sprintf(name,"%s/%s",argv[1],entry->d_name);
+        stat(name,&sobj);
+        if(S_ISREG(sobj.st_mode))
+        {
+            if(iMax < sobj.st_size)
+            {
+                iMax = sobj.st_size;
+                strcpy(namecopy,name);
+            }
+        }
+    }
+
+    printf("Name of largest file : %s with size %d bytes \n",namecopy,iMax);
+
+    closedir(dp);
+    
+    return 0;
+}
